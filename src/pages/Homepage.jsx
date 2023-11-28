beae570679d4172e2907b0e236e12adc95d40103
phill52 <paner225@gmail.com>
2023-11-28 13:50:31 -0500

    create home page

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div className="flex flex-col justify-center align-center h-screen home-grad px-[12%]">
+    <div className="bg-cream rounded-lg text-center px-[12%]">
+      <h2 className="text-darkviolet font-bold font-underline text-4xl pb-4 pt-8">My Project</h2>
+        <div className="bg-lightgreen rounded-[30px] text-center text-cream text-4xl py-3.5">
+          Resume
+        </div>
+        <div className='flex flex-row mt-2 mb-6'>
+          <div>
+            <ul className='text-left text-darkviolet text-4xl w-full'>
+              <li>Last ended at 12:30</li>
+              <li>Total time: 5 hours, 30 minutes</li>
+              <li>Pay rate: 10$</li>
+            </ul>
+          </div>
+
+          <div className='flex flex-col w-[60%]'>
+            <div className='bg-lightorange rounded-[30px] text-3xl mb-2 py-1.5 text-darkviolet'>
+              Invite Another Member
+            </div>
+            <div className='bg-brightyellow rounded-[30px] text-3xl py-16 text-darkviolet'>
+              Switch Project
+            </div>
+          </div>
+    </div>
+  </div>
+  </div>
+  )
+}
